8e7c3e92484bbcf51d7991cdec249f3b6c5f1899
zynlifep <113280546+zynlifep@users.noreply.github.com>
2022-09-11 17:19:20 +0800

    pull commit

--- src/main/java/com/atzyn/git/GitTest.java
+++ src/main/java/com/atzyn/git/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("zz");
         System.out.println("pp");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
