8eb29a7cae1da2a5c218610958baccbd4ac665a0
PP <PP@Zyn.com>
2022-09-11 16:35:03 +0800

    first commit

---
+++ src/main/java/com/atzyn/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atzyn.git;
+
+/**
+ * @author zyn
+ * version  1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
