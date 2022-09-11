ddd00013dfa565c63bec936d11ea919c97850a12
PP <PP@Zyn.com>
2022-09-11 16:50:37 +0800

    hot-fix commit2

--- src/main/java/com/atzyn/git/GitTest.java
+++ src/main/java/com/atzyn/git/GitTest.java
@@ -10,5 +10,7 @@ public class GitTest {
         System.out.println("hello yuu");
         System.out.println("hello uuuhhhhh");
         System.out.println("hello git4!");
+        System.out.println("hello master!");
+        System.out.println("hello hot-fix!");
     }
 }
