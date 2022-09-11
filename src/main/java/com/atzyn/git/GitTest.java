22fb0317452a09abd0af6944152ffee6b780e38b
PP <PP@Zyn.com>
2022-09-11 16:57:16 +0800

    master commit3

--- src/main/java/com/atzyn/git/GitTest.java
+++ src/main/java/com/atzyn/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello git4!");
         System.out.println("hello master!");
         System.out.println("hello hot-fix!");
+        System.out.println("zz");
     }
 }
